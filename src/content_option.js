4737acb3b4fb9121b0e34440759efadc7748626b
Aakhilsk <shaik.21bce9820@vitapstudent.ac.in>
2023-10-17 16:20:03 +0530

    first commit

---
+++ src/content_option.js
@@ -0,0 +1,119 @@
+const logotext = "AK";
+const meta = {
+    title: "{Your name}",
+    description: "I'm Aakhil Shaik, a technologist and cybersecurity enthuist.",
+};
+
+const introdata = {
+    title: "Hi, I'm Aakhil Shaik",
+    animated: {
+        first: "Technologist",
+        second: "Visionary",
+        third: "Data Analyst",
+        fourth: "Programmer",
+        fifth: "Software Developer",
+        sixth: "",
+    },
+    description: "Dedicated technology enthusiast with an insatiable curiosity for unraveling the complexities of our digital world.",
+    your_img_url: "https://i.ibb.co/wRjcnGL/me.jpg",
+};
+
+const dataabout = {
+    title: "Who am I?",
+    aboutme: "I am an undergraduate student currently pursuing a Bachelor's degree in Computer Science at VIT-AP. I specialize in Artificial Intelligence and Machine Learning (AI/ML) and have a strong academic background with a focus on these fields. I have published two research papers, showcasing my research and analytical skills in AI/ML. In addition to my academic achievements, I have completed two significant projects, demonstrating practical applications of AI and ML principles. Moreover, I possess excellent public speaking, leadership, and debating skills, which are essential for effective communication and leadership roles. Beyond my academic and professional interests, I am an avid practitioner of judo, emphasizing discipline and physical fitness, and I have a deep passion for art and creative expression. Music is also a significant part of my life, reflecting my well-rounded personality and appreciation for the arts. This multifaceted background and skill set make me a well-rounded and dynamic individual, capable of excelling in various aspects of life and contributing positively to any endeavor.",
+    };
+const worktimeline = [{
+        jobtitle: "Example 1",
+        where: "Google",
+        date: "2023",
+    },
+    {
+        jobtitle: "Example 2",
+        where: "Micosoft",
+        date: "2022",
+    },
+    {
+        jobtitle: "Example 3",
+        where: "Meta",
+        date: "2021",
+    },
+];
+
+const skills = [{
+        name: "Python",
+    },
+    
+    {
+        name: "C",
+    },
+    
+    {
+        name: "Java",
+    },
+    
+    {
+        name: "Bash",
+    },
+
+    {
+        name: "Powershell",
+    },
+
+    {
+        name: "Machine Learning",
+    },
+
+    {
+        name: "Deep Learning",
+    },
+
+    {
+        name: "Full Stack Development",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://5.imimg.com/data5/GLADMIN/VideoImage/2023/7/328516293/XJ/SW/UV/2822194/patient-health-monitoring-system-using-raspberry-pi-iot-500x500.jpg",
+        description: "Raspberry Pi Health Diagnosis systems leverage compact computing to monitor vital signs and environmental data revolutionizing remote healthcare.",
+        link: "#",
+    },
+    {
+        img: "https://circuitdigest.com/sites/default/files/projectimage_mic/Arduino-Vehicle-Accident-Alert-System-using-GPS-GSM-and-Accelerometer.jpg",
+        description: "Arduino Uno-based smart road safety systems offer real-time monitoring of vehicle movements.instantly alerting authorities and providing valuable data for improved road safety measures.",
+        link: "#",
+    },
+    {
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQn6SgEJu1iChIxhbNt00QW9Bvhf5TkVe_zflxGHNZFOHFQnpX8p-fsEMgBWsWNhl5R4YU&usqp=CAU",
+        description: "I conducted a comprehensive and meticulous literature review on Brain-Computer Interfcae and synthesized the information into a cohesive review paper, ensuring accuracy and clarity in the presentation of findings.",
+        link: "#",
+    },
+    {
+        img: "https://d3i71xaburhd42.cloudfront.net/e349d52fe09c322c8b00bdbf9511cd69d504efdd/1-Figure1-1.png",
+        description: "I rigorously researched EEG classification on the P300 Dataset, developing a hybrid CNN architecture and wrote a paper which reflects my dedication to innovation and precision in the field of brain-computer interfaces.",
+        link: "#",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "aakhilshaik1310@gmail.com",
+    description: "Looking forward to hearing from you!",
+    YOUR_SERVICE_ID: "SERVICE_ID",
+    YOUR_TEMPLATE_ID: "TEMPLATE_ID",
+    YOUR_USER_ID: "USER_ID (PUBLIC_KEY)",
+};
+
+const socialprofils = {
+    github: "https://github.com/Aakhilsk",
+    linkedin: "https://www.linkedin.com/in/aakhil-shaik-568016235/",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
